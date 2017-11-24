8e0d5df01050e4b287850a6c288593b52a172acd
guoleijie <guoleijie@papayamobile.cn>
2017-11-24 13:30:09 +0800

    单元测试 ok

--- test/test_memoize.py
+++ test/test_memoize.py
@@ -36,13 +36,32 @@ class TestMemoize(unittest.TestCase):
     def test_memoize_with_param(self):
         r0, r1, r2 = param_func(1,2), param_func(1,2), param_func(2,2)
         self.assertEqual(r0, r1)
-        self.assertNotEqual(r2, r1)
+        self.assertNotEqual(r1, r2)
 
-    def test_del_memoize(self):
+    def test_del_function_memoize(self):
         r0, r1, _ = param_func(1, 2), param_func(2, 2),delete_memoized('param_func', 1, 2)
         r01,r11 = param_func(1, 2), param_func(2, 2)
         self.assertEqual(r1, r11)
         self.assertNotEqual(r0, r01)
+
+    def test_memoize_diff_instance(self):
+        adder1,adder2 = Adder(), Adder()
+        self.assertNotEqual(adder1.add(3),adder2.add(3))
+
+    def test_delete_memoized(self):
+        adder1, adder2 = Adder(), Adder()
+        a1,a2 = adder1.add(3), adder2.add(3)
+        delete_memoized(adder1.add)
+        a12,a22 = adder1.add(3), adder2.add(3)
+        self.assertEqual(a2,a22)
+        self.assertNotEqual(a1, a12)
+
+        delete_memoized(Adder.add)
+        a112,a222 = adder2.add(3),adder1.add(3)
+        self.assertNotEqual(a112, a12)
+        self.assertNotEqual(a222, a22)
+
+
 
 
 
