7a5d395ceb23b77e798ba55bfae7731110897db6
guoleijie <guoleijie@papayamobile.cn>
2017-12-05 17:02:43 +0800

    增加LRU算法的cache

---
+++ LRUCache.py
@@ -0,0 +1,139 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+
+    def __repr__(self):
+        return "node[val = {},next = {}]".format(self.val,self.next)
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node(None)
+        self.tail = self.head.next
+        self._size = 0
+
+    def add(self,data):
+        node = Node(data)
+        self.tail.next = node
+        self.tail = node
+        self._size += 1
+
+    def insert_head(self,data):
+        node = Node(data)
+        node.next = self.head.next
+        self.head.next = node
+        self._size += 1
+
+    def remove_tail(self):
+        pre = self.head
+        while pre is not None:
+            if pre.next == self.tail:
+                pre.next = None
+                self.tail = pre
+                self._size = self._size - 1
+                return self.tail.val
+            pre = pre.next
+        return False
+
+
+    def size(self):
+        return self._size
+
+
+    def remove(self,data):
+        pre = self.head
+        cur = self.head.next
+        while cur is not None:
+            if cur.val == data:
+                pre.next = cur.next
+                self._size = self._size - 1
+                return True
+            pre,cur = pre.next,cur.next
+        return False
+
+    def traversal(self):
+        cur = self.head.next
+        while cur is not None:
+            print(cur.val,end=" ")
+            cur = cur.next
+
+
+
+
+class LRUCache:
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self._cache = {}
+        self._key_list = LinkedList()
+
+
+
+
+    def _upgrade_frequent(self,key):
+        if self._key_list.remove(key):
+            self._key_list.insert_head(key)
+
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if self._cache.get(key,None) is None:
+            return None
+        value = self._cache[key]
+        self._upgrade_frequent(key)
+        return value
+
+
+
+    def _check_capacity(self):
+        if self._key_list.size() >= self.capacity :
+            key = self._key_list.remove_tail()
+            if key:
+                self._cache.pop(key)
+
+
+
+    def put(self, key, value):
+        """
+        :type key: str
+        :type value: int
+        :rtype: void
+        """
+        if self._cache.get(key,None) is not None:
+            self._upgrade_frequent(key)
+        self._check_capacity()
+        self._key_list.insert_head(key)
+        self._cache[key] = value
+
+
+    @property
+    def cache(self):
+        return self._cache
+
+
+if __name__ == '__main__':
+    lru = LRUCache(capacity=5)
+    for i in range(20):
+       lru.put("key{}".format(str(i)) ,i)
+    print(lru._key_list.traversal())
+    x = lru.get("key{}".format(str(17)))
+    print("=====126===========",x)
+    print(lru._key_list.traversal())
+    x = lru.get("key{}".format(str(18)))
+    print("=====129===========", x)
+    print(lru._key_list.traversal())
+    i = 1
+    lru.put("key{}".format(str(i)), i)
+    print(lru._key_list.traversal())
+
+
+
+
+
+
