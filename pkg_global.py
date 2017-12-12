b2f9be5c615b5f09f76d5eb9b4c99412fe198c6d
guoleijie <guoleijie@papayamobile.cn>
2017-12-12 18:26:00 +0800

    修改日志

--- pkg_global.py
+++ pkg_global.py
@@ -5,14 +5,18 @@ import functools
 import configparser
 import os
 
+logging.basicConfig(level = logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 IMPLEMENTED_CACHE_TYPE = ['simple','lru']
 
 
 def _init_cache_type():
-    config_file = 'config/config.ini'
+    CUR_PATH = os.path.dirname(os.path.abspath(__file__))
+    config_file = CUR_PATH+'\config\config.ini'
     config = configparser.ConfigParser()
     result = config.read(config_file)
-    print("config_file is :",result)
+    logging.info("config_file is : {}".format(result))
+
     if not result:
         acache = SimpleCache()
         return acache
@@ -23,19 +27,20 @@ def _init_cache_type():
     except KeyError:
         raise Exception("There is no 'CacheType' in config file")
 
-    if cache_type is None or cache_type == IMPLEMENTED_CACHE_TYPE[0]:
+    if cache_type is None or cache_type.lower() == IMPLEMENTED_CACHE_TYPE[0]:
         cache_type = 'simple'
         acache = SimpleCache()
-    elif cache_type == IMPLEMENTED_CACHE_TYPE[1]:
+    elif cache_type.lower() == IMPLEMENTED_CACHE_TYPE[1]:
         acache = LRUCache()
     else:
         raise Exception("The Cache type is not implemented")
     return acache
 
 acache = _init_cache_type()
+logging.info("cache type is : {}".format(acache))
 instancekeys = []
 
-print(acache)
+
 
 
 
